1f74358c960153ce32a411255d6ae26202d753ac
AshuRathor <97465069+AshuRathor@users.noreply.github.com>
2023-08-31 12:58:58 +0530

    Add files via upload

--- script2.js
+++ script2.js
@@ -14,23 +14,31 @@ function successCallback(position) {
 
 
 const map = L.map('map', {
-  maxBounds: regionBounds,
+  maxBounds: L.latLngBounds(
+    [[28.61599,77.03292], [28.60558,77.04266]]
+  ),  
   maxBoundsViscosity: 2,
   minZoom:16.4,  
+  // maxZoom: 25
 }).fitBounds(regionBounds);
 
+// var map = L.map('map').setView([51.505, -0.09], 20);
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //   L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
     
-  }).addTo(map);
+  // }).addTo(map);
+
+
   L.marker([latitude, longitude]).addTo(map)
       .bindPopup('That\'s my location ')
       .openPopup();
 
-      const imageBounds = [[28.61599,77.03292], [28.60558,77.04266]];
-      var RMI = "map2.png"
+      // const imageBounds = [[28.61599,77.03292], [28.60558,77.04266]];
+      const imageBounds = [[28.61601231410319, 77.02660944980602], [28.60585675716733, 77.04876803378558]]
+      // var RMI = "map2.png"
+      var RMI = "MapUIBuild.png"
       L.imageOverlay(RMI, imageBounds).addTo(map)
-
 
 }
 
@@ -43,15 +51,6 @@ var options = {
   timeOut: 5000,
   maximumAge: 0
 }
-
-
-
-
-
-
-
-
-
 
 
 
